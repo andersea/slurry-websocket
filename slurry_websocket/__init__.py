1993addf7dcd7df55c3482c7977628ba594845d4
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2020-08-23 15:49:45 +0200

    Bump version: 0.1.2 → 0.1.3

--- slurry_websocket/__init__.py
+++ slurry_websocket/__init__.py
@@ -1,5 +1,5 @@
 """Websocket client section for the Slurry stream processing microframework."""
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from trio_websocket import ConnectionClosed, ConnectionTimeout, HandshakeError, DisconnectionTimeout
 

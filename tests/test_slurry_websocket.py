225ef32037da35bda2865763f25a17fba4a47f7f
Anders Ellenshøj Andersen <andersa@ellenshoej.dk>
2021-05-11 21:42:45 +0200

    Propagate ConnectionClosed errors

--- tests/test_slurry_websocket.py
+++ tests/test_slurry_websocket.py
@@ -2,11 +2,9 @@ import orjson
 import pytest
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed, open_websocket, connect_websocket
-import timeit
 
 from slurry import Pipeline
 from slurry_websocket import Websocket
-
 
 @pytest.fixture
 async def echo_server():
@@ -16,6 +14,28 @@ async def echo_server():
             try:
                 message = await ws.get_message()
                 await ws.send_message(message)
+            except ConnectionClosed:
+                break
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(serve_websocket, handler, '127.0.0.1', 49000, None)
+        yield ('127.0.0.1', 49000)
+        nursery.cancel_scope.cancel()
+
+@pytest.fixture
+async def disconnecting_server():
+    async def handler(request):
+        ws = await request.accept()
+        while True:
+            try:
+                await ws.send_message('bark')
+                await trio.sleep(0.5)
+                await ws.send_message('cart')
+                await trio.sleep(0.5)
+                await ws.send_message('suffer')
+                await trio.sleep(0.5)
+                await ws.send_message('impress')
+                await trio.sleep(0.5)
+                await ws.aclose(1000, 'Plug pulled.')
             except ConnectionClosed:
                 break
     async with trio.open_nursery() as nursery:
@@ -47,14 +67,16 @@ async def test_json(echo_server, hello_world):
         assert 'message' in response
         assert response['message'] == 'hello, world'
 
-async def test_binary(echo_server, hello_world):
-    send_channel, receive_channel = trio.open_memory_channel(1)
-    async with Pipeline.create(
-        receive_channel,
-        Websocket.create('127.0.0.1', 49000, '/', use_ssl=False, parse_json=False) 
-    ) as pipeline, pipeline.tap() as tap:
-        message = orjson.dumps(hello_world)
-        await send_channel.send(message)
-        response = await tap.receive()
-        assert isinstance(response, bytes)
-        assert response == message
+async def test_disconnect(disconnecting_server, autojump_clock):
+    async def listener(tap):
+        async for item in tap:
+            assert item in {'bark', 'cart', 'suffer', 'impress'}
+    try:
+        async with Pipeline.create(
+            Websocket.create('127.0.0.1', 49000, '/', use_ssl=False, parse_json=False) 
+        ) as pipeline, trio.open_nursery() as nursery:
+            nursery.start_soon(listener, pipeline.tap())
+            nursery.start_soon(listener, pipeline.tap())
+    except ConnectionClosed as cls:
+        assert cls.reason.code == 1000
+        assert cls.reason.reason == 'Plug pulled.'
